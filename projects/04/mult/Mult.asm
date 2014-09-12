902122880d267dc43b06e4b6a9d9153268d0eb80
Prasan Samtani <prasan.samtani@hulu.com>
2014-09-12 05:18:55 -0700

    Added assembler program for multiplication

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -6,4 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+	@R1	
+	D=M
+	@R2
+	M=0
+	@R3
+	M=-D
+	(LOOP)
+	@R3
+	D=M
+	@END
+	D;JEQ
+	@R0
+	D=M
+	@R2
+	M=D+M
+	@R3
+	M=M+1
+	@LOOP
+	0;JMP
+	(END)
+	@END
+	0;JMP
+
+
