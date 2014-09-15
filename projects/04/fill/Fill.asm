cc245063f04c279dbb79f1a0cb49880717907d80
Prasan Samtani <prasansamtani@gmail.com>
2014-09-14 17:16:50 -0700

    Finished fill program

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -8,4 +8,49 @@
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
-// Put your code here.
+	@16384
+	D=A
+	@R0
+	M=D
+	@R1
+	M=D
+	@16416
+	D=A
+	@R2
+	M=D
+
+	(CLOOP)
+	@24576
+	D=M
+	@BLOOP
+	D;JNE
+	@R0
+	D=M
+	@R1
+	D=D-M
+	@CLOOP
+	D;JEQ
+	@R0
+	AD=M
+	M=0
+	D=D-1
+	@R0
+	M=D
+	@CLOOP
+	0;JMP
+
+	(BLOOP)
+	@R0
+	D=M
+	@R2
+	D=D-M
+	@CLOOP
+	D;JEQ
+	@R0
+	AD=M
+	M=1
+	D=D+1
+	@R0
+	M=D
+	@CLOOP
+	0;JMP
